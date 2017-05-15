380e05de69dca145259e965babe02fa037cf7ce4
luyiming <luyimingchn@gmail.com>
2017-05-15 23:42:56 +0800

    implement priority_queue

--- include/priority_queue.hpp
+++ include/priority_queue.hpp
@@ -1,128 +1,137 @@
 #ifndef PRIORITY_QUEUE_HPP
 #define PRIORITY_QUEUE_HPP
 
-#include <iostream>
-#include <cassert>
-#include <algorithm>
 #include "Vector.hpp"
+#include "Iterator.hpp"
 
 namespace TinySTL {
 
-    template<typename T>
+    template <typename T, typename Container = TinySTL::vector<T>, typename Compare = std::less<typename Container::value_type>>
     class priority_queue {
     public:
-        const T& top() const;
-        void pop();
-        void push(const T &x);
-        void increaseKey(const T& k, const T& newKey); // O(n) TODO: O(logn)
-        void decreaseKey(const T& k, const T& newKey); // O(n) TODO: O(logn)
-        T& find(const T& k);
+        using value_type = T;
+        using container_type = Container;
+        using reference = typename container_type::reference;
+        using const_reference = typename container_type::const_reference;
+        using size_type = std::size_t;
 
-        bool empty() const;
-        void clear();
-        int size() const;
+    public:
+        priority_queue() = default;
+
+        template <typename InputIterator>
+        priority_queue(InputIterator first, InputIterator last, const Compare& comp = Compare()) 
+            : compare(comp), heap()
+        {
+            heap.assign(first, last);
+            make_heap();
+        }
+
+        bool empty() const { return heap.empty(); }
+
+        size_type size() const { return heap.size(); }
+
+        const_reference top() const { return heap[0]; }
+
+        void pop() {
+            heap[0] = heap[heap.size() - 1];
+            heap.pop_back();
+            siftDown(0, heap.size() - 1);
+        }
+
+        void push(const value_type& val) {
+            heap.push_back(val);
+            siftUp(heap.size() - 1);
+        }
+
+        void push(value_type&& val) {
+            heap.push_back(val);
+            siftUp(heap.size() - 1);
+        }
+
+        template <typename... Args> 
+        void emplace(Args&&... args) {
+            heap.emplace_back(std::forward<Args>(args)...);
+            siftUp(heap.size() - 1);
+        }
+
+        void clear() { heap.clear(); }
+
+        void increaseKey(const T& k, const T& newKey) {
+            int i;
+            for (i = 0; i < heap.size(); i++) {
+                if (heap[i] == k) {
+                    heap[i] = newKey;
+                    break;
+                }
+            }
+            siftDown(i, heap.size() - 1);
+        }
+
+        void decreaseKey(const T& k, const T& newKey) {
+            int i;
+            for (i = 0; i < heap.size(); i++) {
+                if (heap[i] == k) {
+                    heap[i] = newKey;
+                    break;
+                }
+            }
+            siftUp(i);
+        }
+
+        const_reference find(const T& k) {
+            int i;
+            for (i = 0; i < heap.size(); i++) {
+                if (heap[i] == k) {
+                    return heap[i];
+                }
+            }
+            return T();
+        }
+
+        void swap(priority_queue& x) noexcept {
+            heap.swap(x);
+        }
 
     private:
-        TinySTL::vector<T> heap; // min heap
-        void siftDown(int start, int end);
-        void siftUp(int start);
+        void siftDown(size_type start, size_type end) {
+            size_type p = start;
+            while (p < end) {
+                size_type k = 2 * p + 1;
+                if (k >= heap.size())
+                    break;
+                if (k + 1 < heap.size() && compare(heap[k + 1], heap[k]))
+                    k++;
+                if (compare(heap[p], heap[k]))
+                    break;
+                std::swap(heap[k], heap[p]);
+                p = k;
+            }
+        }
+
+        void siftUp(size_type start) {
+            int p = start;
+            while (p > 0) {
+                int k = (p - 1) / 2;
+                if (compare(heap[k], heap[p]))
+                    break;
+                std::swap(heap[k], heap[p]);
+                p = k;
+            }
+        }
+
+        void make_heap() {
+            int start = heap.size() / 2;
+            while (start >= 0) {
+                siftDown(start, heap.size() - 1);
+                start--;
+            }
+        }
+
+    private:
+        Compare compare;
+        container_type heap; // min heap
     };
 
-    template <typename T>
-    const T& priority_queue<T>::top() const {
-        return heap[0];
-    }
-
-    template <typename T>
-    void priority_queue<T>::pop() {
-        heap[0] = heap[heap.size() - 1];
-        heap.pop_back();
-        siftDown(0, heap.size() - 1);
-    }
-
-    template <typename T>
-    void priority_queue<T>::push(const T &x) {
-        heap.push_back(x);
-        siftUp(heap.size() - 1);
-    }
-
-    template <typename T>
-    void priority_queue<T>::increaseKey(const T& k, const T& newKey) {
-        int i;
-        for (i = 0; i < heap.size(); i++) {
-            if (heap[i] == k) {
-                heap[i] = newKey;
-                break;
-            }
-        }
-        siftDown(i, heap.size() - 1);
-    }
-
-    template <typename T>
-    void priority_queue<T>::decreaseKey(const T& k, const T& newKey) {
-        int i;
-        for (i = 0; i < heap.size(); i++) {
-            if (heap[i] == k) {
-                heap[i] = newKey;
-                break;
-            }
-        }
-        siftUp(i);
-    }
-
-    template <typename T>
-    T& priority_queue<T>::find(const T& k) {
-        int i;
-        for (i = 0; i < heap.size(); i++) {
-            if (heap[i] == k) {
-                return heap[i];
-            }
-        }
-        return T();
-    }
-
-    template <typename T>
-    void priority_queue<T>::clear() {
-        heap.clear();
-    }
-
-    template <typename T>
-    bool priority_queue<T>::empty() const {
-        return heap.empty();
-    }
-
-    template <typename T>
-    int priority_queue<T>::size() const {
-        return heap.size();
-    }
-
-    template <typename T>
-    void priority_queue<T>::siftDown(int start, int end) {
-        int p = start;
-        while (p < end) {
-            int k = 2 * p + 1;
-            if (k >= heap.size())
-                break;
-            if (k + 1 < heap.size() && heap[k + 1] < heap[k])
-                k++;
-            if (heap[p] < heap[k])
-                break;
-            std::swap(heap[k], heap[p]);
-            p = k;
-        }
-    }
-
-    template <typename T>
-    void priority_queue<T>::siftUp(int start) {
-        int p = start;
-        while (p > 0) {
-            int k = (p - 1) / 2;
-            if (heap[k] < heap[p])
-                break;
-            std::swap(heap[k], heap[p]);
-            p = k;
-        }
-    }
 
 } // namespace TinySTL 
 
--- test/priority_queueTest.cpp
+++ test/priority_queueTest.cpp
@@ -1,6 +1,9 @@
 #include "priority_queue.hpp"
 #include "gtest/gtest.h"
 
+#include <functional>
+#include <iostream>
+
 TEST(PriorityQueueTest, test1) {
     TinySTL::priority_queue<int> pq;
     EXPECT_TRUE(pq.empty());
@@ -18,3 +21,23 @@ TEST(PriorityQueueTest, test1) {
     EXPECT_TRUE(pq.empty());
 }
 
+TEST(PriorityQueueTest, test2) {
+    TinySTL::vector<int> vec = { 1,3,2,4,0 };
+
+    TinySTL::priority_queue<int> pq1(vec.begin(), vec.end());
+    EXPECT_EQ(pq1.size(), vec.size());
+    for (size_t i = 0; i < vec.size(); i++) {
+        EXPECT_EQ(pq1.top(), i);
+        pq1.pop();
+    }
+    EXPECT_TRUE(pq1.empty());
+
+    TinySTL::priority_queue<int, TinySTL::vector<int>, std::greater<int>> 
+        pq2(vec.begin(), vec.end());
+    EXPECT_EQ(pq2.size(), vec.size());
+    for (size_t i = 0; i < vec.size(); i++) {
+        EXPECT_EQ(pq2.top(), vec.size() - i - 1);
+        pq2.pop();
+    }
+    EXPECT_TRUE(pq2.empty());
+}
